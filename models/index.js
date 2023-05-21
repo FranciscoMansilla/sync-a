5063f8a0155085b85acf334c4e39842ca522411f
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2023-05-21 17:44:25 -0300

    add models path

--- models/index.js
+++ models/index.js
@@ -2,6 +2,10 @@ const { model } = require("mongoose")
 
 const models = {
   User: require('./nosql/users'),
+  Category: require('./nosql/category'),
+  Licence: require('./nosql/licence'),
+  Product: require('./nosql/product'),
+
 }
 
 module.exports = models
